a79d63493c42e8d88fff121e29e6ff64714875da
Brian Mullin <mullin.mm@gmail.com>
2018-02-20 19:28:36 +0100

    whoa

---
+++ Assets/Local Materials/GameListScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class GameListScript : NetworkBehaviour {
+
+	public GameObject ListOfGames;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Local Materials/PlayerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ToggleEvent : UnityEvent<bool>{}
+
+public class PlayerScript : NetworkBehaviour {
+
+	[SerializeField] ToggleEvent onToggleShared;
+	[SerializeField] ToggleEvent OnToggleLocal;
+	[SerializeField] ToggleEvent OnToggleRemote;
+
+	GameObject mainCamera;
+
+	void Start()
+	{
+
+		EnablePlayer();
+	}
+		
+
+	void DisablePlayer()
+	{
+		onToggleShared.Invoke (false);
+
+		if (isLocalPlayer) {
+			OnToggleLocal.Invoke (false);
+		}
+		else
+			OnToggleRemote.Invoke (false);
+	}
+
+	void EnablePlayer()
+	{
+		onToggleShared.Invoke (true);
+
+		if (isLocalPlayer) {
+			OnToggleLocal.Invoke (true);
+
+
+		}
+		 else
+			OnToggleRemote.Invoke (true);
+	}
+}
